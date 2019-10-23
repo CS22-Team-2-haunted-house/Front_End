f4bcd0e9c62d8379fda90da8735639a0c4e9ff21
Brandon Porter <brendlorend@gmail.com>
2019-10-23 14:39:44 -0700

    WIP: Public changes

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,4 +1,4 @@
-import React,{useEffect, useRef, useState,useReducer} from 'react'
+import React,{useEffect, useRef, useState} from 'react'
 import none from '../../images/0.jpg'
 import top from '../../images/1.jpg'
 import right from '../../images/2.jpg'
